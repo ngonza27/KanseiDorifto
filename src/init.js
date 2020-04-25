a960456f14d450d8ddaaa1bd0f8fdab248aee807
ngonza27 <gonzalezvallejonicolas@gmail.com>
2020-04-25 13:14:32 -0500

    Adding coind checkpoint

--- src/init.js
+++ src/init.js
@@ -18,6 +18,15 @@ const config = {
             }
         }
     },
+    plugins: {
+        scene: [
+          {
+            plugin: PhaserMatterCollisionPlugin, // The plugin class
+            key: "matterCollision", // Where to store in Scene.Systems, e.g. scene.sys.matterCollision
+            mapping: "matterCollision" // Where to store in the Scene, e.g. scene.matterCollision
+          }
+        ]
+    },
     scene: {
         preload: preload,
         create: create,
@@ -28,39 +37,64 @@ const config = {
 var game = new Phaser.Game(config);
 var cursors;
 var car;
+var coin;
 var velocity = 0;
 
 function preload() {
-    this.load.image("car", "./assets/carroInicio.png");
+    this.load.image("car", "./assets/car.png");
+    this.load.image("coin", "./assets/coin.png");
     this.load.image('background', './assets/track.png');
 }
 
 function create() {
     background = this.add.image(MAX_WIDTH/2, MAX_HEIGHT/2, 'background');
-    car = this.matter.add.image(600, 200, "car");
+    coin = this.matter.add.image(MAX_WIDTH-MAX_WIDTH/1.5,MAX_HEIGHT/1.21,'coin');
+    car  = this.matter.add.image(MAX_WIDTH-MAX_WIDTH/2.55, MAX_HEIGHT/1.26, "car");
+    
+    car.rotation= 3.15;
     car.setFixedRotation();
     car.setFrictionAir(0.01);
     car.setMass(30);
     this.matter.world.setBounds(0, 0, MAX_WIDTH, MAX_HEIGHT);
-
-    //background.setScale(1.6, 1.9);
-    car.setScale(.2, .2);
+    
+    background.setScale(1.6, 1.9);
+    coin.setScale(.1, .1);
+    car.setScale(.15, .15);
 
     cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+
+    // this.matter.world.on('collisionstart', function (event, coin, car) {
+    //     //this.matter.world.remove(coin);
+    //     console.log("que hubo prro");
+    //     coin.destroy();
+    // });
+
+    // this.matterCollision.addOnCollideStart({
+    //     objectA: car,
+    //     objectB: coin,
+    //     callback: function(eventData) {
+    //         this.matter.world.remove(coin);
+    //         coin.destroy();
+    //     },
+    //     context: this 
+    // });
+
     if (cursors.left.isDown) {
-        car.angle -= 3;
+        car.angle -= 2.5;
     }
     else if (cursors.right.isDown) {
-        car.angle += 3;
+        car.angle += 2.5;
     }
 
     if (cursors.up.isDown) {
-        car.thrustBack(0.0006);
+        //car.thrust(0.0006);
+        car.thrust(0.0004);
     }
     else if (cursors.down.isDown) {
-        car.thrust(0.0006);
+        //car.thrustBack(0.0006);
+        car.thrustBack(0.0004);
     }
 }
